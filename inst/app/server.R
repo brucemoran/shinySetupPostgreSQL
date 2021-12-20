3099bb64ed4a891fc9cc2a52f882db9c812527b0
brucemoran <bruce01campus@gmail.com>
2021-12-20 16:34:35 +0000

    shinySetupPostgreSQL::obsev

--- inst/app/server.R
+++ inst/app/server.R
@@ -16,75 +16,75 @@ function(input, output, session) {
 
   con <- shiny::reactiveValues()
 
-  obsev_userpass(input, con)
+  shinySetupPostgreSQL::obsev_userpass(input, con)
 
   ## allow disconnect as too many open connections (16+)
 
-  obsev_disconnex(input)
+  shinySetupPostgreSQL::obsev_disconnex(input)
 
   ## allow changing of credentials for database
 
-  obsev_db_conxn(input, output)
+  shinySetupPostgreSQL::obsev_db_conxn(input, output)
 
   ## * load table that already exists ------------------------------------------
 
   vals_data <- shiny::reactiveValues()
 
-  obsev_extantabled(input, con, vals_data)
+  shinySetupPostgreSQL::obsev_extantabled(input, con, vals_data)
 
   ## * take file inputs --------------------------------------------------------
 
-  obsev_FILENAMES(input, con, vals_data)
+  shinySetupPostgreSQL::obsev_FILENAMES(input, con, vals_data)
 
-  obsev_go_datared(input, con, vals_data)
+  shinySetupPostgreSQL::obsev_go_datared(input, con, vals_data)
 
   ## * overwrite table with input ----------------------------------------------
 
-  obsev_go_askdata(input, con, vals_data)
+  shinySetupPostgreSQL::obsev_go_askdata(input, con, vals_data)
 
   ## * save current table ------------------------------------------------------
 
-  obsev_save_tab(input)
+  shinySetupPostgreSQL::obsev_save_tab(input)
 
-  obsev_go_save(input, con, vals_data)
+  shinySetupPostgreSQL::obsev_go_save(input, con, vals_data)
 
   ## * add columns to table ----------------------------------------------------
 
-  obsev_add_col(input)
+  shinySetupPostgreSQL::obsev_add_col(input)
 
-  obsev_ins_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_ins_col(input, vals_data)
 
   ## * remove empty columns from table -----------------------------------------
 
-  obsev_del_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_del_col(input, vals_data)
 
-  obsev_col_del(input, vals_data)
+  shinySetupPostgreSQL::obsev_col_del(input, vals_data)
 
   ## * reorder columns in table ------------------------------------------------
 
-  obsev_ord_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_ord_col(input, vals_data)
 
-  obsev_arr_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_arr_col(input, vals_data)
 
   ## * rename columns in table -------------------------------------------------
 
-  obsev_ren_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_ren_col(input, vals_data)
 
-  obsev_rename_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_rename_col(input, vals_data)
 
   ## * output tables -----------------------------------------------------------
 
-  obsev_show_tab(input, vals_data, output)
+  shinySetupPostgreSQL::obsev_show_tab(input, vals_data, output)
 
-  obsev_save_rds(input)
+  shinySetupPostgreSQL::obsev_save_rds(input)
 
-  obsev_go_rds(input, vals_data)
+  shinySetupPostgreSQL::obsev_go_rds(input, vals_data)
 
   ## * unique values of each column selected ---------------------------------
 
-  obsev_uni_col(input, vals_data)
+  shinySetupPostgreSQL::obsev_uni_col(input, vals_data)
 
-  obsev_uniq_col(input, vals_data, output)
+  shinySetupPostgreSQL::obsev_uniq_col(input, vals_data, output)
 
   ## * disconnect from db on session end -------------------------------------
 
