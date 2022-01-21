b27d7e76c90813da326a65170f329ba1b1a34777
brucemoran <bruce01campus@gmail.com>
2022-01-21 18:22:05 +0000

    PDF input

--- R/parsing.R
+++ R/parsing.R
@@ -493,7 +493,7 @@ parse_input <- function(INPUT){
     shiny::showModal(modalDialog("Reading CMD format PDF input, please wait.\n", footer = NULL))
 
     tibList <- lapply(INPUT$FILENAMES$datapath, function(f){
-      import_cmd_pdfs(pdf_path = INPUT$FILENAMES$datapath)
+      import_cmd_pdf(pdf_path = f)
     })
     vals_tib <- do.call(dplyr::bind_rows, tibList)
     vals_tib <- dplyr::distinct(vals_tib)
