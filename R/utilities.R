f99b4c98f4d52bda7f422117f54c7b58810c2cf4
brucemoran <bruce01campus@gmail.com>
2022-01-22 23:23:19 +0000

    ymd

--- R/utilities.R
+++ R/utilities.R
@@ -91,7 +91,7 @@ import_cmd_pdfs <- function (pdf_path) {
           muts <- "NO MUTS"
         }
 
-        return(list(Year = lubridate::year(date_rec),
+        return(list(Year = lubridate::year(lubridate::ymd(date_rec)),
                     Forename = fname,
                     Surname = sname,
                     DOB = dob,
