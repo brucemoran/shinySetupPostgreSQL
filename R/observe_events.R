d66613e97cbfd86fa43a5cba4fd932f4e1a0d9d9
brucemoran <bruce01campus@gmail.com>
2022-01-23 00:02:48 +0000

    ny

--- R/observe_events.R
+++ R/observe_events.R
@@ -179,9 +179,10 @@ obsev_go_askdata <- function(INPUT, CON, VALS_DATA){
       } else {
         is_rec <- "Date_Ext_Rec"
       }
-      print("YEARS")
-      print(lubridate::year(vals_new[,is_rec]))
-#      vals_new[,"Year"] <- lubridate::year(vals_new[,is_rec])
+
+      vals_new[,"Year"] <- unlist(lapply(vals_new[,is_rec], function(f){
+          format(f, format="%Y")
+        })
 
       ##combine
       VALS_DATA$Data <- dplyr::bind_rows(VALS_DATA$Data, vals_new)
